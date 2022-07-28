46e6b4bef8cb4b90fd52fce5b1703d8dbff96ca7
Dmitry Mikheikin <dmitridmitri04052000@gmail.com>
2022-07-28 13:16:14 +0300

    TagRepository is Repo!

--- src/main/java/ru/madeira/onlinelibrarymanagementsystem/repository/TagRepository.java
+++ src/main/java/ru/madeira/onlinelibrarymanagementsystem/repository/TagRepository.java
@@ -1,8 +1,10 @@
 package ru.madeira.onlinelibrarymanagementsystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.madeira.onlinelibrarymanagementsystem.entity.Tag;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Tag findByName(String tagName);
 }
