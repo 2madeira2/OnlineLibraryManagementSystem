18de7199356e28cbc5c0f9bdb991ed110d92cddc
Dmitry Mikheikin <dmitridmitri04052000@gmail.com>
2022-08-16 02:00:09 +0300

    Specifications v2. Works.

--- src/main/java/ru/madeira/onlinelibrarymanagementsystem/specification/BookSpecification.java
+++ src/main/java/ru/madeira/onlinelibrarymanagementsystem/specification/BookSpecification.java
@@ -7,16 +7,14 @@ import ru.madeira.onlinelibrarymanagementsystem.entity.Book;
 import ru.madeira.onlinelibrarymanagementsystem.entity.Genre;
 
 import javax.persistence.criteria.*;
+import java.util.Collection;
 
 public class BookSpecification {
 
     public static Specification<Book> getBookSpecification(BookSearchCriteria bookSearchCriteria) {
         Specification<Book> filter = Specification.where(null);
         if(bookSearchCriteria.getWord() != null) {
-            Specification<Book> bookSpecification = Specification.where(titleOrAuthorContainsWord(bookSearchCriteria.getWord()));
-            bookSpecification = bookSpecification.or(authorSurnameContainsWord(bookSearchCriteria.getWord()));
-            filter = filter.and(bookSpecification);
-            //filter = filter.and(/*titleOrAuthorContainsWord(bookSearchCriteria.getWord()).or(*/authorSurnameContainsWord(bookSearchCriteria.getWord()));
+            filter = filter.and(titleOrAuthorContainsWord(bookSearchCriteria.getWord()).or(authorSurnameContainsWord(bookSearchCriteria.getWord())));
         }
         if(bookSearchCriteria.getMaxYear() != null) {
             filter = filter.and(yearLessThan(bookSearchCriteria.getMaxYear()));
@@ -38,8 +36,10 @@ public class BookSpecification {
     public static Specification<Book> authorSurnameContainsWord(String word) {
         return (root, query, criteriaBuilder) ->
         {
-           Join<Book, Author> bookAuthorJoin = root.join("authors");
-           return criteriaBuilder.like(bookAuthorJoin.get("surname"), "%" + word + "%");
+            query.distinct(true);
+            Root<Author> author = query.from(Author.class);
+            Expression<Collection<Book>> authorsBooks = author.get("books");
+            return criteriaBuilder.and(criteriaBuilder.like(author.get("surname"), word), criteriaBuilder.isMember(root, authorsBooks));
         };
     }
 
@@ -58,34 +58,4 @@ public class BookSpecification {
     public static Specification<Book> yearLessThan(Long maxYear) {
         return (((root, query, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("year"), maxYear)));
     }
-
-
-//    @Override
-//    public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
-//        Predicate predicate = criteriaBuilder.conjunction();
-//        List<Predicate> predicates = new ArrayList<>();
-//        if(bookSearchCriteria != null) {
-//            if(bookSearchCriteria.getGenre() != null) {
-//                Join<Book, Genre> bookGenreJoin = root.join("genres");
-//                predicates.add(criteriaBuilder.like(bookGenreJoin.get("name"), bookSearchCriteria.getGenre()));
-//            }
-//            if(bookSearchCriteria.getMaxYear() != null) {
-//                predicates.add(criteriaBuilder.le(root.get("year"), bookSearchCriteria.getMaxYear()));
-//            }
-//            if(bookSearchCriteria.getMinYear() != null) {
-//                predicates.add(criteriaBuilder.ge(root.get("year"), bookSearchCriteria.getMinYear()));
-//            }
-//            if(bookSearchCriteria.getWord() != null) {
-//                //Join<Book, Author> bookAuthorJoin = root.join("authors");
-//                // Predicate surnamePredicate = criteriaBuilder.like(bookAuthorJoin.get("surname"), "%" + bookSearchCriteria.getAuthorOrTitle() + "%");
-//                //Predicate titlePredicate = criteriaBuilder.like(root.get("title"), "%" + bookSearchCriteria.getAuthorOrTitle() + "%");
-//                // Predicate authorOrTitlePredicate = criteriaBuilder.or(surnamePredicate, titlePredicate);
-//                predicates.add(criteriaBuilder.like(root.get("title"), "%" + bookSearchCriteria.getWord() + "%"));
-//            }
-//        }
-//        for(Predicate predicate1 : predicates) {
-//            predicate = criteriaBuilder.and(predicate1);
-//        }
-//        return predicate;
-//    }
 }
